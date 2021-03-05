77d143ddcbbc183b4ca5e94e1d5f350254d9f431
pfcoder <pfcoder97@gmail.com>
2021-03-05 21:55:00 +0800

    update new lottery method

--- bin/ctt/pallets/kp/src/tests.rs
+++ bin/ctt/pallets/kp/src/tests.rs
@@ -16,5 +16,14 @@ fn kp_account_power() {
         let factor = KpModule::power_factor(200000);
         let converted = KpModule::balance_apply_power(balance, factor);
         assert!(converted == 3252747248u64);
+
+        let test_list = vec![1, 23, 34, 67, 88, 200, 300];
+        assert!(KpModule::binary_search_closet(&test_list, &32) == 2);
+        assert!(KpModule::binary_search_closet(&test_list, &34) == 2);
+        assert!(KpModule::binary_search_closet(&test_list, &88) == 4);
+        assert!(KpModule::binary_search_closet(&test_list, &70) == 4);
+        assert!(KpModule::binary_search_closet(&test_list, &201) == 6);
+        assert!(KpModule::binary_search_closet(&test_list, &0) == 0);
+        assert!(KpModule::binary_search_closet(&test_list, &300) == 6);
     });
 }
